5d0fb9b4b2e97a03757ff15eb193c58421c5d19d
Joe Ardent <code@ardent.nebcorp.com>
2020-01-05 12:23:48 -0800

    Add f32 test, tiny optimization in case len == 2.

--- src/lib.rs
+++ src/lib.rs
@@ -185,11 +185,21 @@ pub fn joe_sort<T: Sortable>(vals: &mut [T]) {
 
 /// Naive merge sort
 fn moej_sort<T: Sortable>(vals: &mut [T], order: &Ordering) {
-    if vals.len() < 2 {
+    let vlen = vals.len();
+
+    if vlen < 2 {
+        return;
+    }
+    if vlen == 2 {
+        if let Some(ord) = vals[1].partial_cmp(&vals[0]) {
+            if ord == *order {
+                vals.swap(0, 1);
+            }
+        }
         return;
     }
 
-    let mid = vals.len() / 2;
+    let mid = vlen / 2;
     let (l_orig, r_orig) = vals.split_at_mut(mid);
     moej_sort(l_orig, order);
     moej_sort(r_orig, order);
@@ -199,7 +209,7 @@ fn moej_sort<T: Sortable>(vals: &mut [T], order: &Ordering) {
     let mut ridx = mid;
     let mut sorted = Vec::with_capacity(vals.len());
 
-    while lidx < mid && ridx < vals.len() {
+    while lidx < mid && ridx < vlen {
         let l = vals[lidx];
         let r = vals[ridx];
         if let Some(ord) = r.partial_cmp(&l) {
@@ -216,7 +226,7 @@ fn moej_sort<T: Sortable>(vals: &mut [T], order: &Ordering) {
     if lidx < mid {
         sorted.extend(&vals[lidx..mid]);
     }
-    if ridx < vals.len() {
+    if ridx < vlen {
         sorted.extend(&vals[ridx..]);
     }
 
@@ -262,8 +272,28 @@ mod tests {
     }
 
     #[test]
-    fn moej_sort_test() {
+    fn moej_sort_ints_test() {
         let mut nums: Vec<i32> = gen_rands(10_000);
+
+        let ushape = Shape::from_slice(&nums);
+
+        // totally unsorted
+        assert!(!ushape.sorted());
+        assert!(!ushape.descending());
+        assert!(!ushape.ascending());
+
+        moej_sort(&mut nums, &std::cmp::Ordering::Less);
+
+        let sshape = Shape::from_slice(&nums);
+
+        assert!(sshape.sorted());
+        assert!(sshape.ascending());
+        assert!(!sshape.descending());
+    }
+
+    #[test]
+    fn moej_sort_floats_test() {
+        let mut nums: Vec<f32> = gen_rands(10_000);
 
         let ushape = Shape::from_slice(&nums);
 
